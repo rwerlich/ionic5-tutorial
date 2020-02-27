51b6c0779532ff4cc3da5661d1d45c170f21cea8
Rodrigo Werlich <rodrigowerlich@clincard.com.br>
2020-02-27 10:00:12 -0300

    removido conteudo desnecessario

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
